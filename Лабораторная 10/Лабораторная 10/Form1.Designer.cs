ebe25053d2b9960b6d08a9e7ef45f1442b09e762
Kozgova <91005974+Kozgova@users.noreply.github.com>
2022-05-13 19:33:44 +0700

    Add files via upload

---
+++ Лабораторная 10/Лабораторная 10/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿
+namespace Лабораторная_10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.View1 = new System.Windows.Forms.DataGridView();
+            this.btStart1 = new System.Windows.Forms.Button();
+            this.View2 = new System.Windows.Forms.DataGridView();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.View1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.View2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.Controls.Add(this.View2);
+            this.panel1.Controls.Add(this.View1);
+            this.panel1.Controls.Add(this.btStart1);
+            this.panel1.Location = new System.Drawing.Point(18, 16);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1403, 588);
+            this.panel1.TabIndex = 1;
+            // 
+            // View1
+            // 
+            this.View1.AllowUserToOrderColumns = true;
+            this.View1.BackgroundColor = System.Drawing.Color.Silver;
+            this.View1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.View1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7});
+            this.View1.Location = new System.Drawing.Point(15, 140);
+            this.View1.Name = "View1";
+            this.View1.RowHeadersWidth = 51;
+            this.View1.RowTemplate.Height = 24;
+            this.View1.Size = new System.Drawing.Size(867, 445);
+            this.View1.TabIndex = 2;
+            // 
+            // btStart1
+            // 
+            this.btStart1.Location = new System.Drawing.Point(408, 32);
+            this.btStart1.Name = "btStart1";
+            this.btStart1.Size = new System.Drawing.Size(636, 73);
+            this.btStart1.TabIndex = 1;
+            this.btStart1.Text = "Узнать результат чемпионата по футболу";
+            this.btStart1.UseVisualStyleBackColor = true;
+            this.btStart1.Click += new System.EventHandler(this.btStart1_Click);
+            // 
+            // View2
+            // 
+            this.View2.BackgroundColor = System.Drawing.Color.Silver;
+            this.View2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.View2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column8,
+            this.Column9,
+            this.Column10});
+            this.View2.Location = new System.Drawing.Point(915, 140);
+            this.View2.Name = "View2";
+            this.View2.RowHeadersWidth = 51;
+            this.View2.RowTemplate.Height = 24;
+            this.View2.Size = new System.Drawing.Size(452, 445);
+            this.View2.TabIndex = 3;
+            // 
+            // Column8
+            // 
+            this.Column8.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column8.HeaderText = "Команда 1";
+            this.Column8.MinimumWidth = 6;
+            this.Column8.Name = "Column8";
+            this.Column8.Width = 107;
+            // 
+            // Column9
+            // 
+            this.Column9.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column9.HeaderText = "Команда 2";
+            this.Column9.MinimumWidth = 6;
+            this.Column9.Name = "Column9";
+            this.Column9.Width = 107;
+            // 
+            // Column10
+            // 
+            this.Column10.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column10.HeaderText = "Результат";
+            this.Column10.MinimumWidth = 6;
+            this.Column10.Name = "Column10";
+            this.Column10.Width = 105;
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column1.HeaderText = "Название команды";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 149;
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column2.HeaderText = "Кол-во игр";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 99;
+            // 
+            // Column3
+            // 
+            this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column3.HeaderText = "Победы";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 89;
+            // 
+            // Column4
+            // 
+            this.Column4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column4.HeaderText = "Ничьи";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 78;
+            // 
+            // Column5
+            // 
+            this.Column5.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column5.HeaderText = "Проигрыши";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 113;
+            // 
+            // Column6
+            // 
+            this.Column6.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column6.HeaderText = "Кол-во голов";
+            this.Column6.MinimumWidth = 6;
+            this.Column6.Name = "Column6";
+            this.Column6.Width = 112;
+            // 
+            // Column7
+            // 
+            this.Column7.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column7.HeaderText = "Кол-во очков";
+            this.Column7.MinimumWidth = 6;
+            this.Column7.Name = "Column7";
+            this.Column7.Width = 114;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1439, 632);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.View1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.View2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btStart1;
+        private System.Windows.Forms.DataGridView View1;
+        private System.Windows.Forms.DataGridView View2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column10;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+    }
+}
+
---
+++ Лабораторная 10/Лабораторная 10/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Лабораторная_10
+{
+    public partial class Form1 : Form
+    {
+
+        Random rnd = new Random();
+        const int TeamAmount = 6;
+        string[] TeamName = new string[TeamAmount] { "Томь", "Спартак", "Динамо", "Челси", "ЦСКА", "Зенит" };
+        
+        const double Win = 2;
+        const double Draw = 1;
+        const double Loss = 0;
+
+        List <Match> MatchesInTournir;
+        Team[] TeamsInTournir;
+         
+        struct Team
+        {
+            public string name;
+            public int game;
+            public int goal;
+            public int win;
+            public int draw;
+            public int loss;
+            public double points;
+        }
+
+        struct Match
+        {
+            public Team team1, team2;
+            public int goal1, goal2;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btStart1_Click(object sender, EventArgs e)
+        {
+            
+            TeamsInTournir = new Team[TeamAmount];
+            MatchesInTournir = new List <Match> ();
+
+            
+            for (int i = 0; i < TeamAmount; i++)
+            {
+                TeamsInTournir[i] = new Team() { name = TeamName[i] };
+            }
+            
+            // матч
+            int goalPlayer1, goalPlayer2;
+            for (int Player1 = 0; Player1 < TeamAmount; Player1++)
+            {
+                for (int Player2 = 0; Player2 < TeamAmount; Player2++)
+                {
+                    if (Player1 != Player2) 
+                    {
+                        goalPlayer1 = GoalsInMatch(rnd.NextDouble()); 
+                        goalPlayer2 = GoalsInMatch(rnd.NextDouble());
+
+                        Match newMatch = new Match() 
+                        {
+                            team1 = TeamsInTournir[Player1],
+                            team2 = TeamsInTournir[Player2],
+                            goal1 = goalPlayer1,
+                            goal2 = goalPlayer2
+                        };
+                        
+                        MatchesInTournir.Add(newMatch); 
+
+                        //данные для таблицы
+                        TeamsInTournir[Player1].goal += goalPlayer1;
+                        TeamsInTournir[Player2].goal += goalPlayer2;
+
+                        TeamsInTournir[Player1].game++;
+                        TeamsInTournir[Player2].game++;
+
+                        if (goalPlayer1 > goalPlayer2)
+                        {
+                            TeamsInTournir[Player1].win++;
+                            TeamsInTournir[Player2].loss++;
+                        }
+                        else if (goalPlayer1 < goalPlayer2)
+                        {
+                            TeamsInTournir[Player1].loss++;
+                            TeamsInTournir[Player2].win++;
+                        }
+                        else
+                        {
+                            TeamsInTournir[Player1].draw++;
+                            TeamsInTournir[Player2].draw++;
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < TeamAmount; i++)
+            {
+                TeamsInTournir[i].points = TeamsInTournir[i].win * Win + TeamsInTournir[i].draw * Draw + TeamsInTournir[i].loss * Loss;
+            }
+
+            //вывод результатов
+            for (int i = 0; i < TeamAmount; i++)
+            {
+                View1.Rows.Add
+                   (TeamsInTournir[i].name,
+                    TeamsInTournir[i].game,
+                    TeamsInTournir[i].win,
+                    TeamsInTournir[i].draw,
+                    TeamsInTournir[i].loss,
+                    TeamsInTournir[i].goal,
+                    TeamsInTournir[i].points);
+            }
+
+            //вывод результатов
+            foreach (var match in MatchesInTournir)
+            {
+                View2.Rows.Add
+                   (match.team1.name,
+                    match.team2.name,
+                    $"{ match.goal1 }:{ match.goal2 }");
+            }
+        }
+
+        // забитые голы
+        private int GoalsInMatch(double l)
+        {
+            int m = 0;
+            double S = Math.Log(rnd.NextDouble() * 10);
+
+            while (S > -l)
+            {
+                S += Math.Log(rnd.NextDouble());
+                m++;
+            }
+
+            return m;
+        }
+
+    }
+    
+}
